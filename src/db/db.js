75a1f04664484623d0285653c5873adaaabf225b
Mihir Chaple <mihirchaple005@gmail.com>
2024-05-22 17:32:01 +0530

    updates in files code and connected to database

--- src/db/db.js
+++ src/db/db.js
@@ -1,23 +1,20 @@
-import mongoose  from "mongoose";
-import express from 'express';
+import mongoose from "mongoose";
+import express from "express";
 import { DB_NAME } from "../constants.js";
 
-const app = express()
+const app = express();
 
-const connetDB = async () => {
+const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("error",(error) => {
-            console.log("Error After connecting : ERR -> ", error);
+        app.on("error", (err) => {
+            console.log("Error connecting to DB => ",err)
         })
-        console.log(`MongoDB connect !! DB Host : ${connectionInstance.connection.host}`)
-        // app.listen(process.env.PORT, () => {
-        //     console.log("App Listning At PORT : ",process.env.PORT)
-        // })
-    } catch (error) {
-        console.log("Unable to connect DataBase : ERR -> ", error)
-        process.exit(1)
+        console.log(`MongoDB connected to the Host : ${connectionInstance.connection.host}`)
+    } catch (err) {
+        console.log("unable to cennect ot DB : ", err);
+        process.exit(1);
     }
 }
 
-export default connetDB
+export default connectDB
