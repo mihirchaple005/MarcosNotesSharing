44c920ad4bd46263db7c58ecbc2e40110999d0b1
mananmaheshwari11 <maheshwarimanan2211@gmail.com>
2024-05-23 17:55:15 +0530

    Create noteModels.js

---
+++ src/models/noteModels.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const noteModel=new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    username:{
+        type:mongoose.ObjectId,
+        ref:"user",
+        required:true
+    },
+    cloud_link:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    }
+},{timestamps:true})
+export default mongoose.model('notes',noteModel)
