249e72cd0395caebde8cf94dc30b9cb532329c71
mananmaheshwari11 <maheshwarimanan2211@gmail.com>
2024-05-23 18:04:38 +0530

    Create and Update noteModels

--- src/models/noteModels.js
+++ src/models/noteModels.js
@@ -17,6 +17,10 @@ const noteModel=new mongoose.Schema({
     description:{
         type:String,
         required:true
+    },
+    branchname:{
+        type:String,
+        required:true
     }
 },{timestamps:true})
 export default mongoose.model('notes',noteModel)
